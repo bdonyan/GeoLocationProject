e009700e0a11562ca6dcadda9d67323bbc16cb38
bdonyan <66976326+bdonyan@users.noreply.github.com>
2021-08-03 22:39:43 -0700

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,27 @@ fs.truncate('locations.json', 0, function(){console.log('done')})
 fs.truncate('addresslist.json', 0, function(){console.log('done')})
 
 app.get('/',(req,res) => {
+    res.sendFile(__dirname + '/frontpage.html') 
+    console.log("description file opened")
+})
+
+app.get('/gmap.html',(req,res) => {
     res.sendFile(__dirname + '/gmap.html') 
+    console.log("HTML file opened")
+})
+
+app.get('/send',(req,res) => {
+    const execSync = require('child_process').execSync;
+        // import { execSync } from 'child_process';  // replace ^ if using ES modules
+        const output = execSync('go run "randomip.go"', { encoding: 'utf-8' });  // the default is 'buffer'
+        console.log('Output was:\n', output);
+    res.download('addresslist.json')
+    res.sendFile(__dirname + '/generateinput.html') 
+    console.log("HTML file opened")
+})
+
+app.get('/generateinput.html',(req,res) => {
+    res.sendFile(__dirname + '/generateinput.html') 
     console.log("HTML file opened")
 })
 
@@ -32,14 +52,14 @@ app.post('/',(req,res) => {
         const output = execSync('go run "ip2location.go"', { encoding: 'utf-8' });  // the default is 'buffer'
         console.log('Output was:\n', output);
 
-        res.redirect("/");
+        res.redirect("/gmap.html");
     }
 })
 
 app.get('/test', function(req, res, next) {
     var obj = JSON.parse(fs.readFileSync('locations.json', 'utf8'));
     res.json(obj);
-    });
+});
 
 app.listen(5000, function() {
     console.log("Server started successfully on port 5000.")
